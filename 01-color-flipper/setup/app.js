219572e3c090403d5d58149f6f587e0164767a3c
hardikpatil24 <24hardikpatil@gmail.com>
2021-06-10 20:39:26 +0530

    Setting up first project "color picker"

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,8 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    // get
+    const randomNumber = 2;
+});
